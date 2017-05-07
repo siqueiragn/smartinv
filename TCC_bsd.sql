b6f799c61b7f3df3af2c5c882120399f40ed35a5
Gabriel <gabrielndesiqueira@hotmail.com>
2017-05-07 00:54:14 -0300

    Atualização arquivo SQL

--- TCC_bsd.sql
+++ TCC_bsd.sql
@@ -1,17 +1,21 @@
 ﻿-- chave estrangeira nas tabelas para computador
 -- barramento_placamae
 
+
+CREATE TABLE computador (
+id_computador serial PRIMARY KEY,
+nome varchar,
+descricao varchar
+);
+
 CREATE TABLE placa_mae (
 id_placa_mae serial PRIMARY KEY,
 nome varchar,
---qtde_sata integer,
---qtde_ide integer,
---qtde_pci integer,
---qtde_pci_e integer,
 socket integer,
 portas_usb integer,
 descricao varchar,
-computador integer
+computador integer, 
+FOREIGN KEY (computador) REFERENTES computador(id_computador)
 );
 CREATE TABLE disco_rigido (
 id_disco_rigido serial PRIMARY KEY,
@@ -19,7 +23,8 @@ nome varchar,
 v_cache integer,
 rpm integer,
 descricao varchar,
-computador integer
+computador integer, 
+FOREIGN KEY (computador) REFERENTES computador(id_computador)
 );
 
 CREATE TABLE processador (
@@ -28,7 +33,8 @@ nome varchar,
 frequencia integer,
 socket varchar,
 descricao varchar,
-computador integer
+computador integer, 
+FOREIGN KEY (computador) REFERENTES computador(id_computador)
 );
 
 CREATE TABLE placa_video (
@@ -38,7 +44,8 @@ frequencia integer,
 memoria integer,
 barramento varchar,
 descricao varchar,
-computador integer
+computador integer, 
+FOREIGN KEY (computador) REFERENTES computador(id_computador)
 );
 
 CREATE TABLE barramento (
@@ -53,7 +60,8 @@ id_memoria serial PRIMARY KEY,
 nome varchar,
 frequencia integer,
 descricao varchar,
-computador integer
+computador integer, 
+FOREIGN KEY (computador) REFERENTES computador(id_computador)
 );
 
 CREATE TABLE fonte (
@@ -61,15 +69,16 @@ id_fonte serial PRIMARY KEY,
 nome varchar,
 potencia integer,
 descricao varchar,
-computador integer
+computador integer, 
+FOREIGN KEY (computador) REFERENTES computador(id_computador)
 );
 
-
-
-
-CREATE TABLE computador (
-id_computador serial PRIMARY KEY,
-nome varchar
+CREATE TABLE barramento_placamae (
+id_barramento_placamae SERIAL PRIMARY KEY,
+id_barramento SERIAL,
+id_placa_mae SERIAL,
+FOREIGN KEY (id_barramento) REFERENCES barramento(id_barramento),
+FOREIGN KEY (id_placa_mae) REFERENCES placa_mae(id_placa_mae)
 );
 
 /*
