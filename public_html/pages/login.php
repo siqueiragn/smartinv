ccacc2a3d46fe1d610c2a81ab9816f01eb5c6162
Gabriel <gabrielndesiqueira@hotmail.com>
2017-05-19 15:26:35 -0300

    Atualização geral Conexão com o banco de dados Criação de páginas modelo Realização de consultas SQL Configuração do CSS geral Configuração do CSS do logo

---
+++ public_html/pages/login.php
@@ -0,0 +1,51 @@
+<?php
+require 'db/DB.class.php';
+$db = new DB();
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TITULO</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+  	<link href="../css/custom.css" rel="stylesheet">
+  	<link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+  </head>
+
+  <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+      <a id="logotitle" class="wrapper" href="#"> SmartInv </a> 
+      </div>
+    </nav>
+    <div class="jumbotron">
+      <div class="container">
+      
+      </div>
+    </div>
+
+    <div class="container">
+        <div class="col-md-12">
+ 		
+ 		</div>
+ 
+
+      <hr>
+
+      <footer>
+        <p>&copy; Gabriel Nunes de Siqueira - SMARTINV, 2017.</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../js/bootstrap.min.js"></script>
+  </body>
+</html>
